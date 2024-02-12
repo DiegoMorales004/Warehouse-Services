15bd425ce00bc71352abd1c40a456eb5073dd407
DiegoMorales004 <diegomoralesss007@gmail.com>
2024-02-12 17:00:14 -0600

    Se agrego el permiso cors

---
+++ src/main/java/com/diegomorales/warehouse/configuration/RestServiceCorsApplication.java
@@ -0,0 +1,18 @@
+package com.diegomorales.warehouse.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class RestServiceCorsApplication {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("/**");
+            }
+        };
+    }
+
+}
